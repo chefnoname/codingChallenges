7dae1041c4bd782e90a0f0afddada6c0a508d7c9
Chef NoName <abdijama21@gmail.com>
2021-07-30 15:47:02 +0100

    update

---
+++ intermediateAlgorithims/sumOfAllPrime.js
@@ -0,0 +1,32 @@
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+
+----------------------------------------------------------------------------------------
+
+
+function sumPrimes(num) {
+    let primeNumSum = 0;
+  
+    for (let i = 2; i <= num; i++) {
+      // console.log(i);
+      if (isPrimeNumber(i)) {
+        primeNumSum += i;
+      }
+    }
+  
+    return primeNumSum;
+  }
+  
+  sumPrimes(10); 
+  
+  function isPrimeNumber(num) {
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  
+    return true;
+  }
